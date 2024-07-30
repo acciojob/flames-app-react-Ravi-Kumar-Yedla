b329fcd84d429a0c74c248bfae14ddf9099f4680
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-07-30 12:13:22 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,68 @@
-import React,{useState}from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 
-const flames = ['Friends','Love','Affection','Marriage','Enemy','Siblings']
+const flames = ['Friends', 'Love', 'Affection', 'Marriage', 'Enemy', 'Siblings']
 
 const App = () => {
-const[name1,setName1] = useState('');
-const[name2,setName2] = useState('');
-const[result,setResult] = useState('');
- 
- const calculateRelationship = () =>{
-    if(name1.trim() === '' || name2.trim() === ''){
-        setResult('Please Enter valid input');
-        return;
+  const [name1, setName1] = useState('');
+  const [name2, setName2] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculateRelationship = () => {
+    if (name1.trim() === '' || name2.trim() === '') {
+      setResult('Please Enter valid input');
+      return;
     }
     let name1Arr = name1.split('');
     let name2Arr = name2.split('');
-    
-    name1Arr.forEach(char =>{
+
+    name1Arr.forEach(char => {
       const index = name2Arr.indexOf(char);
-      if(index !== -1){
-        name2Arr.splice(index,1);
+      if (index !== -1) {
+        name2Arr.splice(index, 1);
       }
     });
 
     const lengthString = name1Arr.length + name2Arr.length;
-    const  remainingString = lengthString % 6 ;
+    const remainingString = lengthString % 6;
 
     setResult(flames[remainingString]);
- }
-          const clearInput = ()=>{
-            setName1('');
-            setName2('');
-            setResult('');
-          }
- 
+  }
+  const clearInput = () => {
+    setName1('');
+    setName2('');
+    setResult('');
+  }
+
 
   return (
     <div id="main">
-      <input type = 'text'
-       placeholder="Enter first name"
-      value = {name1}
-      onChange={(e) => setName1(e.target.value)}
-      data-testid="input1"
+      <input
+        data-testid="input1"
+        type="text"
+        value={name1}
+        onChange={(e) => setName1(e.target.value)}
+        placeholder="Enter first name"
       />
 
-   <input type = 'text'
-       placeholder="Enter second name"
-      value = {name2}
-      onChange={(e) => setName2(e.target.value)}
-      data-testid="input2"
+      <input
+        data-testid="input2"
+        type="text"
+        value={name2}
+        onChange={(e) => setName2(e.target.value)}
+        placeholder="Enter second name"
       />
- <button onClick = {calculateRelationship} data-testid="calculate_relationship">Calculate Relationship Future</button>
- <button onClick={clearInput} data-testid="clear">Clear </button>
- {result && <h3 data-testid="answer">{result}</h3>}
-
+      <button onClick={calculateRelationship} data-testid="calculate_relationship">Calculate Relationship Future</button>
+      <button onClick={clearInput} data-testid="clear">Clear </button>
+      {/* {result && <h3 data-testid="answer">{result}</h3>} */}
+      <h3 data-testid="answer">{result}</h3>
     </div>
   )
 }
 
 export default App
 
-  
+
 
 
 
